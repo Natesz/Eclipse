ab5549981490676970e60cc5582098f5adaeab63
Natesz <natesz777@yahoo.com>
2019-05-24 08:07:01 +0200

    change text

--- eclipse-workspace/HackerRank/algorithms/aVeryBigSum/Solution.java
+++ eclipse-workspace/HackerRank/algorithms/aVeryBigSum/Solution.java
@@ -8,7 +8,7 @@ import java.util.*;
 public class Solution {
 
   public static void main(String[] args) {
-      /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+      /* Enter code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
       Scanner input = new Scanner(System.in);
       int n  = input.nextInt();
       long sum = 0;
